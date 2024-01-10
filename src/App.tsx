308e3ceca5c9e2ff914285e87b0d348d24a8f075
Emma Gerigscott <elgerig@gmail.com>
2024-01-09 17:13:49 -0800

    Add routes for drawings and paintings

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Drawings from './pages/Drawings'
+import Paintings from './pages/Paintings'
 import Splash from './pages/Splash'
 
 const App: React.FC = () => {
@@ -6,6 +8,10 @@ const App: React.FC = () => {
     <Router>
         <Routes>
           <Route path="/" element={<Splash />} />
+          <Route path="/drawings" element={<Drawings />}/>
+          <Route path="/drawings/:drawingId" element={<Drawings />}/>
+          <Route path="/paintings" element={<Paintings />}/>
+          <Route path="/paintings/:paintingId" element={<Paintings />}/>
         </Routes>
     </Router>
   )
