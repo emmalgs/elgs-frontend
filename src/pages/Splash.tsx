775f6a77ffeb5a55019b10ebe80477ef0168a1e2
Emma Gerigscott <elgerig@gmail.com>
2024-01-09 17:13:55 -0800

    Refactor Splash page to include marquee and background

--- src/pages/Splash.tsx
+++ src/pages/Splash.tsx
@@ -1,9 +1,19 @@
 const Splash = () => {
   return (
-    <div>
-      <h1>WELCOME</h1>
+    <div className="splash-main">
+      <div className="marquee">
+        <div className="marquee__content">
+          <span>- EMMA GERIGSCOTT -</span>
+          <span>- EMMA GERIGSCOTT -</span>
+        </div>
+        <div aria-hidden="true" className="marquee__content">
+          <span>- EMMA GERIGSCOTT -</span>
+          <span>- EMMA GERIGSCOTT -</span>
+        </div>
+      </div>
+      <div className="splash-background"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Splash
+export default Splash;
